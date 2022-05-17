09a72ede87e4e3c6518b8f5ac22d23a28d6e9e9f
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-05-16 20:51:35 -0400

    jquery to update navbar items

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function () {
   'use strict';
 
@@ -30,3 +32,21 @@
     carousels();
   })(jQuery);
 })();
+
+$('.navbar-nav').click(function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav-link')) {
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+  let li = $(this).children();
+  li.removeClass('active');
+  console.log(e.target);
+  $(e.target).parent().addClass('active');
+});
+
+$('.navbar-brand').click(function (e) {
+  const id = $(e.target).parent()[0].getAttribute('href');
+  document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+});
