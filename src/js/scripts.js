43bfe6a9b14484ed9b0c835a8ad81f254eaa66f0
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-05-16 21:27:20 -0400

    adding jquery effects

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const navbar = document.querySelector('.navbar-nav');
+
 (function () {
   'use strict';
 
@@ -33,6 +35,22 @@
   })(jQuery);
 })();
 
+
+// changing opacity on navbar items when hovered
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav-link')) {
+    const link = e.target;
+    const siblings = link.closest('.navbar-nav').querySelectorAll('.nav-link');
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+//button click scroll events
 $('.navbar-nav').click(function (e) {
   e.preventDefault();
   if (e.target.classList.contains('nav-link')) {
@@ -48,5 +66,12 @@ $('.navbar-nav').click(function (e) {
 
 $('.navbar-brand').click(function (e) {
   const id = $(e.target).parent()[0].getAttribute('href');
+  document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+});
+
+$('.read-more').click(function (e) {
+  e.preventDefault();
+  const id = e.target.getAttribute('href');
+  console.log(id);
   document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
 });
